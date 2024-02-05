554379e61b205998d1b679c131882d234ca75086
Carla Oliveira <111895486+carla11235813@users.noreply.github.com>
2024-02-04 21:31:24 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,16 +2,68 @@ const btnCriptografar = document.querySelector("#btnCriptografar");
 const btnDescriptografar = document.querySelector("#btnDescriptografar");
 const btnCopiar = document.querySelector("#btnCopiar");
 const textoRecebido = document.querySelector("#texto__recebido");
-const textoEnviado = document.querySelector("#texto__enviado__p");
+const textoEnviado = document.querySelector("#texto__enviado__p"); 
+
+
+/*   FUNÇÕES   */
+
+function verificarLetras(texto) {
+    const letrasAceitas = "abcdefghijklmnopqrstuvwxyz ";
+
+    for (var i = 0; i < texto.length; i++) {
+        if (!letrasAceitas.includes(texto[i])) {
+            alert("Digite Apenas letras minúsculas e sem acentos")
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function criptografar(textoDescriptografado) {
+    substituicoes = {
+        'a': 'ai',
+        'e': 'enter',
+        'i': 'imes',
+        'o': 'ober',
+        'u': 'ufat'
+    }
+
+    let textoCriptografado = textoDescriptografado.split('').map((letra) => substituicoes[letra] || letra).join('');
+
+    textoEnviado.textContent = textoCriptografado;
+}
+
+function descriptografar(textoCriptografado) {
+    substituicoes = {
+        'ai': 'a',
+        'enter': 'e',
+        'imes': 'i',        
+        'ober': 'o',
+        'ufat': 'u'
+    }
+
+    let textoDescriptografado = textoCriptografado.split(/(ai|enter|imes|ober|ufat)/).map((part) => substituicoes[part] || part).join('');
+    textoEnviado.textContent = textoDescriptografado;
+    
+}
+
+
+/*   EVENTOS  */
 
 btnCriptografar.addEventListener("click", function() {
-    console.log(`Criptografar ${textoRecebido.value}`)
-    textoEnviado.textContent = textoRecebido.value;
+    if (verificarLetras(textoRecebido.value)) {
+        criptografar(textoRecebido.value);
+    }
 })
 
 btnDescriptografar.addEventListener("click", function() {
+    verificarLetras(textoRecebido.value);
     console.log(`Descriptografar ${textoRecebido.value}`)
-    textoEnviado.textContent = textoRecebido.value;
+
+    if (verificarLetras(textoRecebido.value)) {
+        descriptografar(textoRecebido.value);
+    }
 })
 
 
@@ -20,8 +72,6 @@ btnCopiar.addEventListener("click", function() {
     
     navigator.clipboard.writeText(textoEnviado.textContent)
     .then(function() {
-      alert("copiado aaaa")
-    })
-    
-    alert("Copiado!")
+      alert("Copiado!")
+    })    
 })
